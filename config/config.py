e5791ed47dd82ae018acdd571af161f04dac83b4
Jeboczek <kanakuba123359@gmail.com>
2022-01-08 01:57:30 +0100

    Replace camelCase to snake_case and add device_name to Config

--- config/config.py
+++ config/config.py
@@ -3,16 +3,21 @@ from typing import Optional
 
 class Config:
     def __init__(self) -> None:
-        self.serverIP : str = "127.0.0.1"
-        self.serverPort : int = 6742
+        self.server_IP : str = "127.0.0.1"
+        self.server_port : int = 6742
+        self.device_name : Optional[str] = None
         self._load_from_env()
 
     def _load_from_env(self):
         # Get IP 
         ip = os.environ.get("IP")
-        self.serverIP = self.serverIP if ip is None else ip
+        self.server_IP = self.server_IP if ip is None else ip
 
-        # Get Port
+        # Get port
         port = os.environ.get("PORT")
-        self.serverPort = self.serverPort if port is None else int(port)
+        self.server_port = self.server_port if port is None else int(port)
+
+        # Get device name
+        device_name = os.environ.get("DEVICE_NAME")
+        self.device_name = device_name
 
--- tests/test_config.py
+++ tests/test_config.py
@@ -6,9 +6,18 @@ class TestConfig(TestCase):
     @mock.patch.dict(os.environ, {"IP": "111.111.111.111"})
     def test_config_ip(self):
         conf = Config()
-        self.assertEqual(conf.serverIP, "111.111.111.111")
+        self.assertEqual(conf.server_IP, "111.111.111.111")
 
     @mock.patch.dict(os.environ, {"PORT": "11111"})
     def test_config_port(self):
         conf = Config()
-        self.assertEqual(conf.serverPort, 11111)
+        self.assertEqual(conf.server_port, 11111)
+
+    @mock.patch.dict(os.environ, {"DEVICE_NAME": "TEST_DEVICE"})
+    def test_config_device_name(self):
+        conf = Config()
+        self.assertEqual(conf.device_name, "TEST_DEVICE")
+
+    def test_config_device_name_without_env(self):
+        conf = Config()
+        self.assertEqual(conf.device_name, None)
